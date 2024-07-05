6c33a4c7dc32ad6183c52910bfe0a6b31779a2d9
Omurbek <shaiurbekuuluomurbek@gmail.com>
2024-07-05 17:41:12 +0600

    first commit

---
+++ src/main/java/com/example/sqlcon/SqlConApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqlcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlConApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlConApplication.class, args);
+    }
+
+}
