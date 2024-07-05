a9b63fdd0252c544374b82058f33c6f5895a5b06
Omurbek <shaiurbekuuluomurbek@gmail.com>
2024-07-05 17:56:37 +0600

    first commit

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,22 @@
+package models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private  String name;
+    private String email;
+}
